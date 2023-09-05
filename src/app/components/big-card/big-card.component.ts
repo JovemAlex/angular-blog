3dea273f2e92de6d92be7b86ddaa1f7e0ddc7d23
Alexsandro <alexsandrolima991@hotmail.com>
2023-09-05 00:03:55 -0300

    (chore): Cria componente Title e Big-Card dentro da página HOME

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css']
+})
+export class BigCardComponent implements OnInit {
+
+  imageCard: string = 'https://cdn1.epicgames.com/0584d2013f0149a791e7b9bad0eec102/offer/GTAV_EGS_Artwork_2560x1440_Landscaped%20Store-2560x1440-79155f950f32c9790073feaccae570fb.jpg'
+  title: string = 'Grand Theft Auto V'
+  palceholder: string = 'GTA V'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
